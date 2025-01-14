4989c7f14966648e9aa9287e157f4b4ac8a9a79c
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-14 17:39:29 -0600

    Adding the other HTTP Methods POST PUT DELETE

--- index.js
+++ index.js
@@ -8,10 +8,29 @@ let app = express ();
 
 // Setting up the HTTP Methods
 
-// GET Method to retrieve the data | The method takes two parameters: (1) Location (2) Request and Response Function.
+// GET Method to retrieve the data from the server | The method takes two parameters: (1) Location (2) Request and Response Function.
 app.get('/username', (req, res) => {
     res.send ("Hello Mustafa Alani!") // Sendign a reponse "Hello Mustafa Alani!" to the client after the request.
 })
+
+
+// POST Method to send the data to the server | The method takes two parameters: (1) Location (2) Request and Response Function.
+app.post('/create', (req, res) => {
+    res.send ("Created!") // Sendign a reponse "Hello Mustafa Alani!" to the client after the request.
+})
+
+// PUT Method to Edit/Add the data to the server | The method takes two parameters: (1) Location (2) Request and Response Function.
+app.put('/update', (req, res) => {
+    res.send ("updated!") // Sendign a reponse "Hello Mustafa Alani!" to the client after the request.
+})
+
+
+// DELETE Method to delete data to the server | The method takes two parameters: (1) Location (2) Request and Response Function.
+app.delete('/delete', (req, res) => {
+    res.send ("Deleted") // Sendign a reponse "Hello Mustafa Alani!" to the client after the request.
+})
+
+
 
 
 // Starting the server by listening to the calls through a port. Local port is (3000).
